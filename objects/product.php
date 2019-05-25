a9bc4f638fb515fcf125565f9e005e8a0751729c
ekunkoya.seun@gmail.com <ekunkoya.seun@gmail.com>
2019-05-25 21:50:52 +0100

    addded the read method in the product class

--- objects/product.php
+++ objects/product.php
@@ -17,6 +17,29 @@ class  Product{
     //constructor with $db as database connection
     public function __construct($db){
     	$this->conn = $db;
-    }
+    }//
+
+    //read producs
+    function read(){
+
+    	//select all query
+    	$query = "SELECT
+                c.name as category_name, p.id, p.name, p.description, p.price, p.category_id, p.created
+            FROM
+                " . $this->table_name . " p
+            LEFT JOIN
+                    categories c
+            ON p.category_id = c.id
+            ORDER BY
+                p.created DESC";
+
+        //prepare query statement
+         $stmt = $this->conn->prepare($query);
+
+         //execute query
+         $stmt->execute();
+
+         return $stmt;
+    }//
 }
 ?>
